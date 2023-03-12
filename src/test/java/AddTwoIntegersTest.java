5ba970728162a670260da660b49faa23d8f1d65a
svulupal <svulupal@opentext.com>
2023-03-12 19:02:34 +0530

    Updated test

--- src/test/java/AddTwoIntegersTest.java
+++ src/test/java/AddTwoIntegersTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class AddTwoIntegersTest {
-}
+import org.junit.jupiter.api.Test;
+
+public class AddTwoIntegersTest {
+    @Test
+    public void testAddIntegers()
+            {
+                int Num1 = 10;
+                int Num2 = 20;
+                int sum = Num1 + Num2;
+                System.out.println(Num1 + " + " + Num2 + " = "  + sum);
+            }
+ }
+
+
