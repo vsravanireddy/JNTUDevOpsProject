9e3be8b99aafdbbf80184a97bc77ed84edb56fbb
svulupal <svulupal@opentext.com>
2023-03-15 20:31:53 +0530

    Added new test for string reversal

---
+++ src/test/java/StringReversalTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+
+public class StringReversalTest {
+    public static void main(String[] args) {
+        String str = "This is my project for JNTU";
+        String reversedStr = stringReversal(str);
+        System.out.println("Given String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }
+    @Test
+    public static String stringReversal(String str) {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+}
+
+
