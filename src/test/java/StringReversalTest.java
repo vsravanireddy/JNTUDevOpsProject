65f6b7c30e4f51ef8b2724fe7c5c21923cefc116
svulupal <svulupal@opentext.com>
2023-03-16 08:09:41 +0530

    Updated test/java/StringReversalTest.java

--- src/test/java/StringReversalTest.java
+++ src/test/java/StringReversalTest.java
@@ -4,23 +4,16 @@ import org.junit.jupiter.api.Test;
 public class StringReversalTest {
 
     @Test
-    public static void main(String[] args) {
-        String str = "This is my project for JNTU";
-        String reversedStr = stringReversal(str);
-        System.out.println("Given String: " + str);
-        System.out.println("Reversed String: " + reversedStr);
-    }
 
-    public static String stringReversal(String str) {
-        if (str == null || str.length() == 0) {
-            return str;
-        }
+    public void  stringReversal() {
+        String str = "This is my project for JNTU";
+        System.out.println("Given String: " + str);
 
         StringBuilder sb = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; i--) {
             sb.append(str.charAt(i));
         }
-        return sb.toString();
+        System.out.println("Reversed String: " + sb);
     }
 }
 
